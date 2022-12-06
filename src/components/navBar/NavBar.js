14c9e7a122b49c887e2f5a7c21c39eb2a0fa7ea2
lourdes-miazzo <lourdesmiazzo@gmail.com>
2022-12-06 12:58:36 -0300

    cambio de link por navLink

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,21 +1,21 @@
 import iconoLourdes from "./iconoLourdes.png"
 import "./navBar.css"
 import CartWidget from "../Cart/CartWidget"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 const NavBar = () => {
   return (
     <>
     <nav>
       <div className='ordenGral'>
-          <Link to="/"><img src={iconoLourdes} className="iconoL" alt='logo de tienda Lourdes Miazzo Obras'/></Link>
+          <NavLink to="/"><img src={iconoLourdes} className="iconoL" alt='logo de tienda Lourdes Miazzo Obras'/></NavLink>
           <ul>
-              <li><Link to="/category/2022">2022</Link></li>
-              <li><Link to="/category/2021">2021</Link></li>
-              <li><Link to="/category/2020">2020</Link></li> 
-              <li><Link to="/category/2019">2019</Link></li>
-              <li><Link to="/category/2018">2018</Link></li>
-              <li><Link to="/category/2017">2017</Link></li> 
-              <li><Link to="/category/2016">2016</Link></li>
+              <li><NavLink to="/category/2022">2022</NavLink></li>
+              <li><NavLink to="/category/2021">2021</NavLink></li>
+              <li><NavLink to="/category/2020">2020</NavLink></li> 
+              <li><NavLink to="/category/2019">2019</NavLink></li>
+              <li><NavLink to="/category/2018">2018</NavLink></li>
+              <li><NavLink to="/category/2017">2017</NavLink></li> 
+              <li><NavLink to="/category/2016">2016</NavLink></li>
           </ul>
           <CartWidget />
       </div>
